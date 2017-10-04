27823d66342b7c0c34f1843959c88f51dd66268c
jacobcook1995 <jc2017@ic.ac.uk>
2017-10-04 10:16:01 +0100

    First commit

---
+++ Week1/Code/MyExampleScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+msg1="Hello"
+msg2=$USER
+echo "$msg1 $msg2"
+
+echo "Hello $USER"
+echo
---
+++ Week1/Code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author: Jacob Cook jc2017@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2017
+
+echo -e "\nThis is a shell script! \n" #what does -e do?
+
+#exit
